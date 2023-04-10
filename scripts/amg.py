3fe06815e3b0de39f1362f89f8dd712554ccae2e
82magnolia <82magnolia@snu.ac.kr>
2023-04-10 22:11:34 +0900

    Added amg

--- scripts/amg.py
+++ scripts/amg.py
@@ -12,6 +12,8 @@ import argparse
 import json
 import os
 from typing import Any, Dict, List
+import matplotlib.pyplot as plt
+import numpy as np
 
 parser = argparse.ArgumentParser(
     description=(
@@ -192,6 +194,23 @@ def get_amg_kwargs(args):
     return amg_kwargs
 
 
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    polygons = []
+    color = []
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        img = np.ones((m.shape[0], m.shape[1], 3))
+        color_mask = np.random.random((1, 3)).tolist()[0]
+        for i in range(3):
+            img[:,:,i] = color_mask[i]
+        ax.imshow(np.dstack((img, m*0.35)))
+
+
 def main(args: argparse.Namespace) -> None:
     print("Loading model...")
     sam = sam_model_registry[args.model_type](checkpoint=args.checkpoint)
@@ -219,7 +238,10 @@ def main(args: argparse.Namespace) -> None:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         masks = generator.generate(image)
-
+        plt.figure(figsize=(20,20))
+        plt.imshow(image)
+        show_anns(masks)
+        plt.show()
         base = os.path.basename(t)
         base = os.path.splitext(base)[0]
         save_base = os.path.join(args.output, base)
